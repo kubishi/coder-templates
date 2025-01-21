5ca1779f762c683ace01066110db6b1d996f5cc5
Jared Coleman <jaredraycoleman@gmail.com>
2025-01-21 11:15:33 -0800

    Updates deeplearning-nvidia template

--- deeplearning-nvidia/main.tf
+++ deeplearning-nvidia/main.tf
@@ -1,20 +1,18 @@
 terraform {
   required_providers {
     coder = {
-      source  = "coder/coder"
-      version = "~>0.12.0"
+      source = "coder/coder"
     }
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "~>3.0.2"
+      source = "kreuzwerker/docker"
     }
   }
 }
-
 locals {
   jupyter-count     = data.coder_parameter.jupyter.value == "false" ? 0 : 1
   code-server-count = data.coder_parameter.code-server.value == "false" ? 0 : 1
   ngc-version       = "24.04"
+  username          = data.coder_workspace_owner.me.name
 }
 
 data "coder_parameter" "ram" {
@@ -109,9 +107,9 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-provider "coder" {}
-
+data "coder_provisioner" "me" {}
 data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
 
 # jupyter
 resource "coder_app" "jupyter" {
@@ -125,15 +123,30 @@ resource "coder_app" "jupyter" {
   share        = "owner"
 }
 
+# resource "coder_app" "code-server" {
+#   count        = local.code-server-count
+#   agent_id     = coder_agent.main.id
+#   display_name = "VS Code Web"
+#   slug         = "code-server"
+#   url          = "http://localhost:8000?folder=/home/${local.username}/data/"
+#   icon         = "https://raw.githubusercontent.com/matifali/logos/main/code.svg"
+#   subdomain    = true
+#   share        = data.coder_parameter.share_vscode_web.value == "false" ? "owner" : "authenticated"
+# }
 resource "coder_app" "code-server" {
-  count        = local.code-server-count
   agent_id     = coder_agent.main.id
-  display_name = "VS Code Web"
   slug         = "code-server"
-  url          = "http://localhost:8000?folder=/home/coder/data/"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/code.svg"
-  subdomain    = true
-  share        = data.coder_parameter.share_vscode_web.value == "false" ? "owner" : "authenticated"
+  display_name = "code-server"
+  url          = "http://localhost:13337/?folder=/home/${local.username}"
+  icon         = "/icon/code.svg"
+  subdomain    = false
+  share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 5
+    threshold = 6
+  }
 }
 
 resource "coder_app" "filebrowser" {
@@ -143,28 +156,34 @@ resource "coder_app" "filebrowser" {
   slug         = "filebrowser"
   url          = "http://localhost:8080/"
   icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
-  subdomain    = true
+  subdomain    = false
   share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:8080/health"
+    interval  = 5
+    threshold = 6
+  }
 }
 
 resource "coder_agent" "main" {
   arch                   = "amd64"
   os                     = "linux"
-  startup_script_timeout = 180
+  # startup_script_behavior = "non-blocking"
   startup_script         = <<EOT
     #!/bin/bash
     set -euo pipefail
 
     # Create user data directory
-    mkdir -p /home/coder/data
+    mkdir -p /home/${local.username}/data
     # make user share directory
-    mkdir -p /home/coder/share
+    mkdir -p /home/${local.username}/share
 
     # Install and start filebrowser
     echo "Installing filebrowser"
     curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
     echo "Starting filebrowser"
-    filebrowser --noauth --root /home/coder/data >/dev/null 2>&1 &
+    filebrowser --noauth --root /home/${local.username}/data >./filebrowser.log --baseurl /@${local.username}/${data.coder_workspace.me.name}/filebrowser 2>&1 &
   
     # Start jupyter
     if [ ${data.coder_parameter.jupyter.value} == true ];
@@ -176,12 +195,14 @@ resource "coder_agent" "main" {
     # Install and satrt VS code-server
     if [ ${data.coder_parameter.code-server.value} == true ];
     then
-      echo "Installing VS Code Web"
-      mkdir -p /tmp/code-server
-      HASH=$(curl https://update.code.visualstudio.com/api/commits/stable/server-linux-x64-web | cut -d '"' -f 2)
-      wget -O- https://vscode.download.prss.microsoft.com/dbazure/download/stable/$HASH/vscode-server-linux-x64-web.tar.gz | tar -xz -C /tmp/code-server --strip-components=1 >/dev/null 2>&1
-      echo "Starting VS Code Web"
-      /tmp/code-server/bin/code-server --accept-server-license-terms serve-local --without-connection-token --telemetry-level off >/dev/null 2>&1 &
+      echo "Installing VS Code Web"      
+      # Install the latest code-server.
+      # Append "--version x.x.x" to install a specific version of code-server.
+      curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server
+
+      # Start code-server in the background.
+      /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+
     fi
 
     # Personalize
@@ -194,10 +215,10 @@ resource "coder_agent" "main" {
     EOT
 
   env = {
-    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
-    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
-    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
-    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
+    GIT_AUTHOR_NAME     = coalesce(data.coder_workspace_owner.me.full_name, data.coder_workspace_owner.me.name)
+    GIT_AUTHOR_EMAIL    = "${data.coder_workspace_owner.me.email}"
+    GIT_COMMITTER_NAME  = coalesce(data.coder_workspace_owner.me.full_name, data.coder_workspace_owner.me.name)
+    GIT_COMMITTER_EMAIL = "${data.coder_workspace_owner.me.email}"
   }
 
   metadata {
@@ -272,6 +293,7 @@ resource "docker_image" "deeplearning" {
     tag        = ["matifali/ngc-${data.coder_parameter.framework.value}:${local.ngc-version}", "matifali/ngc-${data.coder_parameter.framework.value}:latest"]
     build_args = {
       "NGC_VERSION" = "${local.ngc-version}"
+      "USERNAME"    = "${local.username}"
     }
     pull_parent = true
   }
@@ -284,22 +306,117 @@ resource "docker_image" "deeplearning" {
 #Volumes Resources
 #home_volume
 resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-home"
+  name = "coder-${data.coder_workspace.me.id}-home"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
 }
 
 #usr_volume
 resource "docker_volume" "usr_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-usr"
+  # name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-usr"
+  name = "coder-${data.coder_workspace.me.id}-usr"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
 }
 
 #etc_volume
 resource "docker_volume" "etc_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-etc"
+  # name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-etc"
+  name = "coder-${data.coder_workspace.me.id}-etc"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
 }
 
 #opt_volume
 resource "docker_volume" "opt_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-opt"
+  # name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-opt"
+  name = "coder-${data.coder_workspace.me.id}-opt"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
 }
 
 resource "docker_container" "workspace" {
@@ -307,9 +424,9 @@ resource "docker_container" "workspace" {
   image    = docker_image.deeplearning.image_id
   memory   = data.coder_parameter.ram.value * 1024
   gpus     = "all"
-  name     = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  hostname = lower(data.coder_workspace.me.name)
-  dns      = ["1.1.1.1"]
+  name     = "coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)}"
+  hostname = data.coder_workspace.me.name
+  # dns      = ["1.1.1.1"]
   command  = ["sh", "-c", coder_agent.main.init_script]
   env      = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
   restart  = "unless-stopped"
@@ -335,11 +452,11 @@ resource "docker_container" "workspace" {
     ip   = "host-gateway"
   }
 
-  ipc_mode = "host"
+  # ipc_mode = "host"
 
   # users home directory
   volumes {
-    container_path = "/home/coder"
+    container_path = "/home/${local.username}"
     volume_name    = docker_volume.home_volume.name
     read_only      = false
   }
@@ -360,13 +477,13 @@ resource "docker_container" "workspace" {
   }
   # users data directory
   volumes {
-    container_path = "/home/coder/data/"
-    host_path      = "/data/${data.coder_workspace.me.owner}/"
+    container_path = "/home/${local.username}/data/"
+    host_path      = "/data/${data.coder_workspace.me.name}/"
     read_only      = false
   }
   # shared data directory
   volumes {
-    container_path = "/home/coder/share"
+    container_path = "/home/${local.username}/share"
     host_path      = "/data/share/"
     read_only      = true
   }
@@ -374,18 +491,20 @@ resource "docker_container" "workspace" {
   # Add labels in Docker to keep track of orphan resources.
   labels {
     label = "coder.owner"
-    value = data.coder_workspace.me.owner
+    value = data.coder_workspace_owner.me.name
   }
   labels {
     label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
+    value = data.coder_workspace_owner.me.id
   }
   labels {
     label = "coder.workspace_id"
     value = data.coder_workspace.me.id
   }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
   labels {
-    label = "coder.workspace_name"
+    label = "coder.workspace_name_at_creation"
     value = data.coder_workspace.me.name
   }
 }
